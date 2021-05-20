ac767b886363bdc9f0d40916c9ac77fe5f75e259
seikhou <seikhoudanfakha@gmail.com>
2021-05-20 10:48:27 +0000

    danfakha coomit

---
+++ OriginalGitProject/src/main/java/com/saraya/OriginalGitProject/OriginalGitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.saraya.OriginalGitProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriginalGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OriginalGitProjectApplication.class, args);
+	}
+
+}
---
+++ OriginalGitProject/src/test/java/com/saraya/OriginalGitProject/OriginalGitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saraya.OriginalGitProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginalGitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
